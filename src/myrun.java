a1a5ca8fe49bc262269b80494982e31a6302e5db
dyziob <dyziob65@gmail.com>
2023-01-14 16:10:23 +0100

    git config --global user.name "Krzysztof Brzostowski" git config --global user.email dyziob65@gmail.com

---
+++ src/myrun.java
@@ -0,0 +1,19 @@
+import java.time.LocalDate;
+
+public class myrun extends Thread {
+    private int id;
+
+    public myrun(int id) {
+        this.id = id;
+    }
+
+    public void run() {
+        System.out.println(Thread.currentThread().getId() + "\tWątek: " + id + "\t" + LocalDate.now());
+        try {
+            Thread.sleep(300);
+
+        } catch (InterruptedException e) {
+        }
+    }
+}
+
